5050c7ff1d01e41a8301cc930671a95250925a4d
anhndh-vbee <anhndh.vbee@gmail.com>
2023-05-17 10:16:14 +0700

    Update checkin

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const config = {
-    PORT: process.env.PORT,
-    HOST_NAME: process.env.HOST_NAME,
-    MONGODB_URL: process.env.MONGODB_URL,
-    JWT_ACCESS_KEY: process.env.JWT_ACCESS_KEY,
-    SLACK_TOKEN: process.env.SLACK_TOKEN,
-    BASE_URL: process.env.BASE_URL,
-    IP: process.env.IP
-}
+  PORT: process.env.PORT,
+  HOST_NAME: process.env.HOST_NAME,
+  MONGODB_URL: process.env.MONGODB_URL,
+  JWT_ACCESS_KEY: process.env.JWT_ACCESS_KEY,
+  SLACK_TOKEN: process.env.SLACK_TOKEN,
+  BASE_URL: process.env.BASE_URL,
+  IP: process.env.IP,
+};
 
 module.exports = config;
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-const obj = {};
-obj["day"] = 2;
-console.log(obj);
+// get week of date
+function getWeek(date) {
+  const weekStart = new Date(date.getFullYear(), 0, 1);
+  const diff = date.getTime() - weekStart.getTime();
+  return Math.floor(diff / (7 * 24 * 60 * 60 * 1000)) + 1;
+}
