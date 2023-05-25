aa8e50af1ad0b97346a7c1daeb7d8d954b46bc1a
bachhoang0606 <0606bach@gmail.com>
2023-05-25 23:51:49 +0700

    refactor route file and delete stylesheet late chart

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,18 +4,18 @@ const router = express.Router();
 const dashboardRoute = require('./dashboard.route');
 const downloadRoute = require('./download.route');
 const homeRoue = require('./home.route');
-router.post('/', userController.saveUserFromSlack);
 const asyncMiddleware = require('../middlewares/async.middleware');
 const { adminAuthorization } = require('../middlewares/auth.middleware');
 const handlerSlackTextMiddleware = require('../middlewares/handlerSlackText.middleware');
 
+router.use(homeRoue);
 router.post('/', userController.saveUserFromSlack);
 router.post('/checkin', userController.checkIn);
 router.get('/user-checkin/:token', userController.postCheckIn);
 
 router.post('/user/schedule', userController.schedule);
 
-// router.use(adminAuthorization);
+router.use(adminAuthorization);
 
 router.post(
   '/users/timekeeping',
@@ -28,9 +28,7 @@ router.post(
   asyncMiddleware(userController.destroy),
 );
 router.post('/users', asyncMiddleware(userController.index));
-
 router.use('/dashboard', dashboardRoute);
 router.use(downloadRoute);
-router.use(homeRoue);
 
 module.exports = router;
